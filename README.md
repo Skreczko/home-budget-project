da7344552e5921ea83d2644634d17c43377eba00
Dawid Skreczko <dawid.skreczko@gmail.com>
2018-09-27 23:33:03 +0200

    repaired django restframework

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 "# home-budget-project" 
 
+<<<<<<< HEAD
 Its an aplication to create expenses for 2 users for each model.
 
 RESTAPI created
 
 i.e. http://127.0.0.1:8000/expenses/
+=======
+Its an aplication to create expends for 2 users for each model.
+>>>>>>> parent of 0876e37... v2 - djangorestframework
--- budget/urls.py
+++ budget/urls.py
@@ -1,20 +1,12 @@
-from django.conf.urls import url, include
-from rest_framework import routers
+
 from django.urls import path
 from . import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'projects', views.ProjectViewSet)
-router.register(r'expenses', views.ExpenseViewSet)
-
 
 urlpatterns = [
     path('', views.project_list, name="list"),
     path('add', views.ProjectCreateView.as_view(), name="add"),
     path('delete/<int:id>', views.delete_expense, name="delete"),
     path('edit/<int:id>', views.edit_budget, name="edit_budget"),
-    path('<slug:project_slug>', views.project_detail, name="detail"),
+    path('<slug:project_slug>', views.project_detail, name="detail"),  #to identity which project i want to currently vue
 
-    url(r'^', include(router.urls)),
 ]
